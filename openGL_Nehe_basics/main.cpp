e62bfa66d3ca43c58848e6d2be1c5b5324466f31
amdreallyfast <amdreallyfast@hotmail.com>
2014-06-02 12:41:35 -0700

    finished the basic framework
    There is still an outstanding issue in which the window's title is a
    bunch of squares instead of English characters.  I am guessing that
    there is a type problem resulting from a forced cast somewhere.

--- openGL_Nehe_basics/main.cpp
+++ openGL_Nehe_basics/main.cpp
@@ -510,131 +510,102 @@ LRESULT CALLBACK my_window_proc(
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-static const UINT SCREEN_WIDTH_PIXELS = 800;
-static const UINT SCREEN_HEIGHT_PIXELS = 600;
-
-
-// this is the main message handler for the program
-LRESULT CALLBACK my_window_proc(
-   HWND window_handle,
-   UINT message,
-   WPARAM w_param, //??the hey is this??
-   LPARAM l_param) //??the hey is this??)
-{
-   switch (message)
-   {
-   case WM_DESTROY:
-      // this message is delivered to the program when the window is clossed
-      PostQuitMessage(0);
-      return 0;
-      // no need for break because "return" is just before it
-   default:
-      break;
-   }
-
-   // call the default window procedure to process any messages that we are not
-   // explicitly handling
-   return DefWindowProc(window_handle, message, w_param, l_param);
-}
-
-int WINAPI WinMain(HINSTANCE h_instance,
+// the entry point of our application
+int WINAPI WinMain(
+   HINSTANCE h_instance,
    HINSTANCE h_prev_instance,
    LPSTR lp_cmd_line,
-   int n_show_cmd)
+   int n_cmd_show
+   )
 {
-   // this will be the handle for the window
-   HWND window_handle;
+   // a window message structure
+   MSG msg;
 
-   // this struct will hold information for the window class
-   WNDCLASSEX wc;
-   ZeroMemory(&wc, sizeof(WNDCLASSEX));
+   // variable to exit program loop
+   bool done = false;
 
-   // fill the strict with behavioral information
-   wc.cbSize = sizeof(WNDCLASSEX);           // ??why? is this legacy stuff??
-   wc.style = CS_HREDRAW | CS_VREDRAW;       // redraw on vertical or horizatial resize
-   wc.lpfnWndProc = my_window_proc;          // a function pointer to our custom window message handling function
-   wc.hInstance = h_instance;                // the handle to our applicaiton instance
-   wc.hCursor = LoadCursor(NULL, IDC_ARROW); // no application handle; default mouse pointer
-   wc.lpszClassName = L"WindowClass1";       // name for window class
-
-   RegisterClassEx(&wc);
-
-   // calculate the window size necessary to enclose our desired-size window 
-   // rectangle
-   RECT window_rectangle =
+   // optional startup message to run program in fullscreen or window mode
+   if (IDNO ==
+      MessageBox(NULL, L"Would you like to run in fullscreen mode?", L"Start Fullscreen?", MB_YESNO | MB_ICONQUESTION))
    {
-      0,                      // origin is (0,0) (origin doesn't matter because we
-      0,                      // only need the width and heigh for calculations)
-      SCREEN_WIDTH_PIXELS,
-      SCREEN_HEIGHT_PIXELS
-   };
-   AdjustWindowRectEx(
-      &window_rectangle,      // pointer to a rectangle
-      WS_OVERLAPPEDWINDOW,    // default window options (necessary for calculations)
-      FALSE,                  // no menus
-      NULL);                  // no "extended window" styles
-
-   // create the window on the screen
-
-   window_handle = CreateWindowEx(
-      NULL,                         // no extra style options
-      L"WindowClass1",              // name of the window class that this handle will refer to
-      L"'Hello World' Win32 Window Program", // displayed on window's title bar
-      WS_OVERLAPPEDWINDOW,          // several default window options
-      300,                          // X origin relative to desktop
-      300,                          // Y origin relative to desktop
-      window_rectangle.right - window_rectangle.left, // window width
-      window_rectangle.bottom - window_rectangle.top, // window height
-      NULL,                         // no parent window
-      NULL,                         // no menus
-      h_instance,                   // identifies window with this application
-      NULL);                        // no multiple windows
-
-   // display window on screen
-   ShowWindow(window_handle, n_show_cmd);
-
-   // enter the main loop
-
-   // this struct holds Windows event message
-   MSG event_message = { 0 };
-
-   // enter the loop and stay here forever (or at least until the window closes)
-   while (true)
+      g_fullscreen = false;
+   }
+   else
    {
-      // check to see if any messages are in the queue
-      // Note: Remove them if there are any, then handle them
-      if (PeekMessage(&event_message, NULL, 0, 0, PM_REMOVE))
+      g_fullscreen = true;
+   }
+
+   //if (!create_GL_window("NeHe's openGL Framework", 640, 480, 16, g_fullscreen))
+   if (!create_GL_window("bildge", 640, 480, 16, g_fullscreen))
+   {
+      return 0;
+   }
+
+   while (!done)
+   {
+      // check if there are messages
+      if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
       {
-         TranslateMessage(&event_message);
-         DispatchMessage(&event_message);
-
-         if (WM_QUIT == event_message.message)
+         // some kind of message exists
+         if (WM_QUIT == msg.message)
          {
-            break;
+            // we received a "quite" message, such as the window being closed 
+            // through it's "X" icon
+            done = true;
+         }
+         else
+         {
+            // don't need to quite, so translate the message (may do nothing 
+            // depending on the message) and send it to message processor for 
+            // whatever window it is meant for
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+         }
+      }
+      else
+      {
+         // no messages, so draw the scene and check for key messages
+         if (g_active)
+         {
+            if (g_keys[VK_ESCAPE])
+            {
+               done = true;
+            }
+            else
+            {
+               // not time to quit, so update the screen
+               draw_GL_scene();
+               SwapBuffers(g_device_context_handle);
+            }
+         }
+
+         // use the "F1" key to switch between fullscreen and not fullscreen
+         if (g_keys[VK_F1])
+         {
+            // kill the current window and recreate it in the other mode
+            g_keys[VK_F1] = false;
+            kill_GL_window();
+            g_fullscreen = !g_fullscreen;
+            if (!create_GL_window("NeHe's openGL Framework", 640, 480, 16, g_fullscreen))
+            {
+               return 0;
+            }
          }
       }
    }
 
-   
-   return event_message.wParam;
+
+   // try to kill the openGL window (assuming one is open) and exit the program
+   kill_GL_window();
+   return (msg.wParam);
 }
+
+
+
+
+
+
+
+
+
+
