4cba1a096fcf69de07557cf8783bff8f210e1357
amdreallyfast <amdreallyfast@hotmail.com>
2014-09-12 09:17:20 -0700

    Made some naming changes to the vertex and fragment shaders to clarify which values were in model and world space, but now the plane isn't lighting up.

--- Jamie_King_Qt_Graphics_Pad/vertex_shader.glsl
+++ Jamie_King_Qt_Graphics_Pad/vertex_shader.glsl
@@ -1,26 +1,27 @@
 #version 430
 	
-in layout(location = 0) vec3 vertex_position;
+in layout(location = 0) vec3 vertex_position_model;
 in layout(location = 1) vec3 vertex_color;
-in layout(location = 2) vec3 vertex_normal;
+in layout(location = 2) vec3 vertex_normal_model;
 
 uniform mat4 world_to_projection_matrix;
 uniform mat4 model_to_world_matrix;
 
-uniform vec3 ambient_light;
 
 // these outputs are for the lighting that is done in the fragment shader
-out vec3 vertex_out_normal;
-out vec3 vertex_out_position;  // this is for the fragment shader so that it can interpolate the points on the triangle for each angle calculation
+out vec3 vertex_position_world;
+out vec3 vertex_normal_world;
+
 
 void main()
 {
-   vec4 position_vec4 = vec4(vertex_position, 1.0f);  // let the position be translatable
-   vec4 normal_vec4 = vec4(vertex_normal, 0.0f);      // do not let the normal be translated
+   // let the position be translatable and the normal not translatable
+   vec4 position_world = model_to_world_matrix * vec4(vertex_position_model, 1.0f);  
+   vec4 normal_world = model_to_world_matrix * vec4(vertex_normal_model, 0.0f);
 
-   gl_Position = world_to_projection_matrix * model_to_world_matrix * position_vec4;
+   gl_Position = world_to_projection_matrix * position_world;
 
-   vertex_out_normal = vec3(model_to_world_matrix * normal_vec4);
-   vertex_out_position = vec3(model_to_world_matrix * position_vec4);
+   vertex_position_world = vec3(position_world);
+   vertex_normal_world = vec3(normal_world);
 }
 
